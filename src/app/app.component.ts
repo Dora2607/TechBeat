9c7138c50608a09edd5c377ab5f303dcac0f50ec
Dora <dora.sparagno@gmail.it>
2024-08-13 15:53:51 +0200

    added og tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'TechBeat';
+export class AppComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private title: Title,
+  ) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('TechBeat');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'TechBeat is an application that allows you to view the latest available news on various topics.',
+      },
+      {
+        name: 'keywords',
+        content:
+          'news, techBeat, latest news, tech news, updated news, various topics, news exploration, news link, news date, news title',
+      },
+      {
+        property: 'og:title',
+        content: 'TechBeat',
+      },
+      {
+        property: 'og:description',
+        content:
+          'techBeat is an application that allows you to view the latest available news on various topics.',
+      },
+      { property: 'og:image', content: 'URL of the Open Graph image' },
+      { property: 'og:url', content: 'URL of your page' },
+    ]);
+  }
 }
